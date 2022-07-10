de3d4f9728a46a125a8a3da59de87937beb90145
ugo.buonadonna <ugo@elliptic.co>
2022-07-11 00:27:21 +0100

    Challenges for Javascript courrse part 2

---
+++ 02-Fundamentals-Part-2/starter/shit.js
@@ -0,0 +1,17 @@
+//Challenge 1 of Fundemantals 2//
+const calAverage1 = (score, score2, score3) => (score, score2, score3) / 3
+const calAverage2 = (score, score2, score3) => (score, score2, score3) / 3 
+calAverage1(44,23,71);
+console.log (calAverage1)
+/*const Dolphins = (44,23,71)
+const Koalas = (65,54,49)
+const CheckWinner= function (DolphinsAverage, KoalasAverage) {
+
+}
+const DolphinsAverage = (44, 23, 71)
+const KoalasAverage = (65, 54, 49)
+
+const DolphinsWin = DolphinsAverage>KoalasAverage
+   if (DolphinsWin = true) {
+    console.log(`Dolphins win ${DolphinsAverage} vs ${KoalasAverage}`)
+    return DolphinsWin */ 
