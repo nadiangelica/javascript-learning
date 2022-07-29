02be212d63eadea648f9fcddb238214f831d4238
Ugo <ugo@elliptic.co>
2022-07-29 12:52:22 +0200

    what does your notes fucking mean?

--- 02-Fundamentals-Part-2/starter/shit.js
+++ 02-Fundamentals-Part-2/starter/shit.js
@@ -1,17 +1,23 @@
-//Challenge 1 of Fundemantals 2//
-const calAverage1 = (score, score2, score3) => (score, score2, score3) / 3
-const calAverage2 = (score, score2, score3) => (score, score2, score3) / 3 
-calAverage1(44,23,71);
-console.log (calAverage1)
-/*const Dolphins = (44,23,71)
-const Koalas = (65,54,49)
-const CheckWinner= function (DolphinsAverage, KoalasAverage) {
+//Challenge 1 of Fundemantals 2      a >= 2*b     >= means 'at least' //
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3
+let Dolphinsaverage1 = calcAverage(44, 23, 71)
+let KoalasAverage1 = calcAverage(65, 54, 49)
 
+function CheckWinner(Dolphinsaverage, KoalasAverage) {
+
+   if (Dolphinsaverage >= 2 * KoalasAverage) {
+      console.log(`Dolphins win ${Dolphinsaverage} vs ${KoalasAverage}`)
+      return
+   }
+   else if (KoalasAverage >= 2 * Dolphinsaverage) {
+      console.log(`Koalas win ${KoalasAverage} vs ${Dolphinsaverage}`)
+      return
+   }
+   else { console.log(' No one wins') }
 }
-const DolphinsAverage = (44, 23, 71)
-const KoalasAverage = (65, 54, 49)
 
-const DolphinsWin = DolphinsAverage>KoalasAverage
-   if (DolphinsWin = true) {
-    console.log(`Dolphins win ${DolphinsAverage} vs ${KoalasAverage}`)
-    return DolphinsWin */ 
+console.log(CheckWinner(Dolphinsaverage1, KoalasAverage1))
+
+Dolphinsaverage1 = calcAverage(85, 54, 41)
+KoalasAverage1 = calcAverage(23, 34, 27)
+console.log(CheckWinner(Dolphinsaverage1, KoalasAverage1))
